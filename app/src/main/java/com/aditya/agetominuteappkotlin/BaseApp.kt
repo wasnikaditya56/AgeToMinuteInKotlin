08351947e007d2d82b30600dae2b7b90042190f4
Aditya <wasnikaditya56@gmail.com>
2021-09-04 21:19:15 +0530

    Initial commit

---
+++ app/src/main/java/com/aditya/agetominuteappkotlin/BaseApp.kt
@@ -0,0 +1,8 @@
+package com.aditya.agetominuteappkotlin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApp  : Application(){
+}
---
+++ app/src/main/java/com/aditya/agetominuteappkotlin/utils/Constant.kt
@@ -0,0 +1,8 @@
+package com.aditya.agetominuteappkotlin.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showMsg(msg:String){
+    Toast.makeText(this,msg, Toast.LENGTH_LONG).show()
+}
